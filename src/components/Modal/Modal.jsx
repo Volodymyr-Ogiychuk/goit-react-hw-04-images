86ef8d7fa4f9b57f016f7138dce16265296f32fe
Volodymyr-Ogiychuk <volodymyr.ogiychuk@gmail.com>
2022-11-09 23:05:32 +0200

    Add new files

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { createPortal } from 'react-dom';
+import PropTypes from 'prop-types';
+import s from '../../styles.module.css';
+
+const modalRoot = document.getElementById('modal-root');
+
+export default class Modal extends Component {
+  
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleCloseByEsc);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleCloseByEsc);
+  }
+
+  handleCloseByEsc = evt => {
+    if (evt.code === 'Escape') {
+      this.props.onCloseModal();
+    }
+  };
+
+  handleCloseByClick = evt =>
+    evt.target === evt.currentTarget && this.props.onCloseModal();
+
+render() {
+    return (createPortal(
+        <div className={s.Overlay} onClick={this.handleCloseByClick}>
+            <div className={s.Modal}>
+                <img src={this.props.url} alt={this.props.alt} />
+            </div>
+        </div>,
+      modalRoot
+    )
+    )
+  }
+}
+
+Modal.propTypes = {
+  onCloseModal: PropTypes.func.isRequired,
+  url: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired,
+};
